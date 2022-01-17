4a291f2719aecc51632580facf59e8758266301d
khuslee <c21io1053@ufe.edu.mn>
2022-01-17 10:48:59 +0800

    shoo hayah

--- dice-game/app.js
+++ dice-game/app.js
@@ -7,18 +7,29 @@ var activePlayer = 1;
  // тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
  var roundscore = 0;
 
-var dice = Math.floor(Math.random() * 6) + 1;
+
 
 
 //document.querySelector("#score-0").textContent = dice;
 
 //document.querySelector("#score-1").textContent = dice;
 
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
+document.getElementById("score-0").textContent = 0;
+document.getElementById("score-1").textContent = 0;
 
 
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
 
-document.querySelector(".dice").style.display="none";
+var diceDom = document.querySelector(".dice");
+diceDom.style.display="none";
+
+
+document.querySelector(".btn-roll").addEventListener("click", function(){
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+    diceDom.style.display="block";
+    diceDom.src = "dice-" + diceNumber + ".png";
+  
+});
+
